8b0398a87ef0f01446f1438421c290abbc9f42b9
s2quake <s2quake@ntreev.com>
2014-07-31 19:15:23 +0900

    1

--- Ntreev.Library.PsdParser/PSD.cs
+++ Ntreev.Library.PsdParser/PSD.cs
@@ -62,6 +62,11 @@ namespace Ntreev.Library.PsdParser
             get { return this.layers; }
         }
 
+        public LinkedLayer[] LinkedLayers
+        {
+            get { return this.linkedLayers; }
+        }
+
         public int Width
         {
             get { return this.fileHeader.Width; }
