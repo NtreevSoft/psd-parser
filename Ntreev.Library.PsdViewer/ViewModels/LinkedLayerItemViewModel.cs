540a9ee23b69a3a5392932411ee7a8ccdd73f1b7
s2quake <s2quake@ntreev.com>
2014-12-22 18:44:24 +0900

    누락 파일 추가

---
+++ Ntreev.Library.PsdViewer/ViewModels/LinkedLayerItemViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Ntreev.Library.Psd;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Ntreev.Library.PsdViewer.ViewModels
+{
+    class LinkedLayerItemViewModel : TreeViewItemViewModel
+    {
+        private readonly ILinkedLayer linkedLayer;
+
+        static int i = 0;
+
+        public LinkedLayerItemViewModel(ILinkedLayer linkedLayer, PSDItemViewModel parent)
+            : base(parent)
+        {
+            this.linkedLayer = linkedLayer;
+
+            foreach (var item in linkedLayer.Document.Childs)
+            {
+                this.Children.Add(new LayerItemViewModel(item, parent));
+            }
+
+
+        }
+
+        public override string Text
+        {
+            get
+            {
+                return string.Format("LinkedLayer({0})", this.linkedLayer.Document.Name);
+            }
+        }
+    }
+}
