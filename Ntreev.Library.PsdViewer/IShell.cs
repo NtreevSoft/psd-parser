911500f97014a293da6304d7d06d03da0e332eec
s2quake <s2quake@ntreev.com>
2018-05-03 09:48:31 +0900

    프로젝트 정리 및 기능 약간 개선

--- Ntreev.Library.PsdViewer/IShell.cs
+++ Ntreev.Library.PsdViewer/IShell.cs
@@ -1,6 +1,6 @@
 //Released under the MIT License.
 //
-//Copyright (c) 2018 Ntreev Soft co., Ltd.
+//Copyright (c) 2015 Ntreev Soft co., Ltd.
 //
 //Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
 //documentation files (the "Software"), to deal in the Software without restriction, including without limitation the 
